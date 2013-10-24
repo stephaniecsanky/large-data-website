038a3a63d6a8658f5b7a3278b6e79ce1c3a0f148
Stephanie Csanky <csan0001@algonquinlive.com>
2013-10-23 23:40:48 -0400

    Changed dog table

--- js/breeds.js
+++ js/breeds.js
@@ -1,33 +1,63 @@
-fillTable = function(){
-	alert("rawr");
+fillTable = function(page){
+	
+	//config
+	var tableName = "dogTable";
+	
+	//pagination variables
+	var ELEMS_PER_PAGE = 10;
+	var start = ELEMS_PER_PAGE * (page - 1);
+	
 	var dogs = [];
 	
-	var airedele = {};
-	airedele.name = "Airedele Terrior";
-	airedele.country = "England";
-	airedele.classification = "Terrior";
-	airedele.image = "//placehold.it/100x100";
+	addDog = function(name, country, classification, image){
+		var dog = {};
+		dog.name = name;
+		dog.country = country;
+		dog.classification = classification;
+		dog.image = image;
+		dogs.push(dog);
+	};
 	
-	dogs.push(airedele); //add airedele to array
 	
-	var akita = {};
-	akita.name = "Akita";
-	akita.country = "Japan";
-	akita.classification = "Utility";
-	akita.image = "http://placehold.it/100x100";
 	
-	dogs.push(akita); //add akita to array
+	addDog("Airedele Terrior", "England", "Terrior", "//placehold.it/100x100");
+	//airedele.name = "Airedele Terrior";
+	//airedele.country = "England";
+	//airedele.classification = "Terrior";
+	//airedele.image = "//placehold.it/100x100";
 	
-	alert(dogs.length);
+	//dogs.push(airedele); //add airedele to array
 	
+	addDog("Akita", "Japan", "Utility", "//placehold.it/100x100");
+	//var akita = {};
+	//akita.name = "Akita";
+	//akita.country = "Japan";
+	//akita.classification = "Utility";
+	//akita.image = "http://placehold.it/100x100";
+	
+	//dogs.push(akita); //add akita to array
+	
+	addDog("Alaskan Malamute", "United States", "Northern Breed", "//placehold.it/100x100");
+	
+	addDog("Alaskan Malamute", "United States", "Northern Breed", "//placehold.it/100x100");
+
+	addDog("Alaskan Malamute", "United States", "Northern Breed", "//placehold.it/100x100");
+	
+	addDog("Alaskan Malamute", "United States", "Northern Breed", "//placehold.it/100x100");
+	
+	var pages = dogs.length / ELEMS_PER_PAGE;
+	if(dogs.length % ELEMS_PER_PAGE > 0) pages++;
+	if(page < 0 || page > pages) return;
+	
+	$("#" + tableName).remove("tr");
 	$(dogs).each(function(i, e){
-		var id = "dog" + i;
-		alert(id);
-		alert(typeof e);
-		$("#dogTable").append("<tr id='" + id + "'></tr>");
-		$("#" + id).append("<td>" + e.name + "</td>");
-		$("#" + id).append("<td>" + e.country + "</td>");
-		$("#" + id).append("<td>" + e.classification + "</td>");
-		$("#" + id).append("<td><img src='" + e.image + "' alt='" + e.name + "'/></td>");
+		if(i >= start && i < start + ELEMS_PER_PAGE){
+			var id = "dog" + i;
+			$("#" + tableName).append("<tr id='" + id + "'></tr>");
+			$("#" + id).append("<td>" + e.name + "</td>");
+			$("#" + id).append("<td>" + e.country + "</td>");
+			$("#" + id).append("<td>" + e.classification + "</td>");
+			$("#" + id).append("<td><img src='" + e.image + "' alt='" + e.name + "'/></td>");
+		}
 	});
 };
